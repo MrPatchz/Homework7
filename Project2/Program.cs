272cf7ecb30c6ada847c92f76d49042a70dd3c27
Jacques Zwielich <moshhowl@gmail.com>
2020-10-26 19:36:29 -0700

    Add project 2

---
+++ Project2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Project2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperature noarg = new Temperature();
+            Temperature oneScaleArg = new Temperature('f');
+            Temperature oneTempArg = new Temperature((float)20.0);
+            Temperature twoArg = new Temperature(30, 'C');
+            Console.Out.WriteLine("~~~~~~~~~~~~Test getTemp~~~~~~~~~~~~~~~");
+            Console.Out.WriteLine(twoArg.getTemperatureF());
+            Console.Out.WriteLine(oneTempArg.getTemperatureC());
+            Console.Out.WriteLine("~~~~~~~~~~~~Test object creation~~~~~~~~~~~~~~~");
+            noarg.ToString();
+            oneTempArg.ToString();
+            oneScaleArg.ToString();
+            twoArg.ToString();
+            Console.Out.WriteLine("~~~~~~~~~~~~Test setters~~~~~~~~~~~~~~~");
+            oneScaleArg.setTemp((float)67.0);
+            oneTempArg.setScale('f');
+            noarg.setAll((float)67.0, 'f');
+            noarg.ToString();
+            oneTempArg.ToString();
+            oneScaleArg.ToString();
+            twoArg.ToString();
+            Console.Out.WriteLine("~~~~~~~~~~~~Test equals~~~~~~~~~~~~~~~");
+            Console.Out.WriteLine(noarg.Equals(oneScaleArg) + " Should be true");
+            Console.Out.WriteLine(noarg.Equals(oneTempArg) + " Should be false");
+
+
+
+        }
+    }
+}
---
+++ Project2/Temperature.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project2
+{
+    class Temperature
+    {
+        private float temperature;
+        private char scale;
+
+        public Temperature()
+        {
+            this.temperature = 0;
+            this.scale = 'C';
+        }
+
+        public Temperature(char scale)
+        {
+            this.temperature = 0;
+            this.scale = scale;
+        }
+
+        public Temperature(float temperature)
+        {
+            this.temperature = temperature;
+            this.scale = 'C';
+        }
+
+        public Temperature(float temperature, char scale)
+        {
+            this.temperature = temperature;
+            this.scale = scale;
+        }
+
+        public float getTemperatureC()
+        {
+            if (this.scale == 'c' || this.scale == 'C')
+            {
+                return this.temperature;
+            }
+            return (float)Math.Round(((this.temperature) - 32.0 * (5.0 / 9.0)), 1);
+        }
+
+        public float getTemperatureF()
+        {
+            if (this.scale == 'f' || this.scale == 'F')
+            {
+                return this.temperature;
+            }
+            return (float)Math.Round((((this.temperature) * (9.0 / 5.0) + 32)), 1);
+        }
+
+        public void setTemp(float temperature)
+        {
+            this.temperature = temperature;
+        }
+
+        public void setScale(char scale)
+        {
+            this.scale = scale;
+        }
+
+        public void setAll(float temperature, char scale)
+        {
+            this.temperature = temperature;
+            this.scale = scale;
+        }
+
+        public void ToString()
+        {
+            Console.Out.WriteLine("The Temperature is "
+                + temperature + " " + scale + "Degrees"
+                );
+        }
+
+        public bool Equals(Temperature compare)
+        {
+            if(this.getTemperatureC() == compare.getTemperatureC())
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
