0c84fd467678d774d7aca5248c72b907b9a1b8fc
Jacques Zwielich <moshhowl@gmail.com>
2020-10-26 18:47:22 -0700

    Add project files.

---
+++ Project1/Odometer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project1
+{
+    class Odometer
+    {
+        private double fuel_usage;
+        private double miles_driven;
+
+        public Odometer()
+        {
+            this.fuel_usage = 0;
+            this.miles_driven = 0;
+        }
+
+        public Odometer(double fuel_usage, double miles_driven)
+        {
+            this.fuel_usage = fuel_usage;
+            this.miles_driven = miles_driven;
+        }
+
+        public void reset()
+        {
+            this.fuel_usage = 0;
+            this.miles_driven = 0;
+        }
+
+        public void add(double fuel_usage, double miles_driven)
+        {
+            this.fuel_usage += fuel_usage;
+            this.miles_driven += miles_driven;
+        }
+
+        public void ToString()
+        {
+            Console.Out.WriteLine("Fuel used:" + this.fuel_usage +
+                "\nMiles Driven:" + this.miles_driven);
+        }
+    }
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Odometer test_default = new Odometer();
+            test_default.add(4.4, 7.0);
+            test_default.ToString();
+            test_default.reset();
+            test_default.ToString();
+
+            Odometer test_2_arg = new Odometer(100,200);
+            test_2_arg.ToString();
+            test_2_arg.add(50, 70);
+            test_2_arg.ToString();
+            test_2_arg.reset();
+            test_2_arg.ToString();
+        }
+    }
+}
